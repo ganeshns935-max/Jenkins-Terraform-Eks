1e54e33eeb2a7508e5f5a18e3535b23684217c00
ramesha <ramesha@1233>
2026-03-19 20:05:36 +0530

    kkk

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"   # ✅ THIS FIXES YOUR ERROR
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 locals {
-  region = "ap-south-1"
+  region = "us-east-1"
   name   = "kastro-eks-cluster"
   vpc_cidr = "10.123.0.0/16"
 
-  azs = ["ap-south-1a", "ap-south-1b"]
-
+  azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
